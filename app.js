3e46c850f627ad5fbe9df3c842195dcc697cea5f
mohamed zakaria <mohamed_zakaria@gmail.com>
2026-07-04 21:41:23 +0300

    added: add signup

---
+++ app.js
@@ -0,0 +1,59 @@
+import express from "express";
+import mysql from "mysql2";
+import "dotenv/config";
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+    return;
+  }
+  console.log("Connected to the database");
+});
+
+app.use(express.json());
+
+app.post("/signup", (req, res) => {
+  try {
+    const { firstName, lastName, email, password, gender, DOB } = req.body;
+
+    db.execute(
+      "SELECT u_email FROM user WHERE u_email = ?",
+      [req.body.email],
+      (err, results) => {
+        if (err) {
+          return res.status(500).json({ error: "Database error" });
+        }
+
+        if (results.length > 0) {
+          return res.status(400).json({ error: "Email already exists" });
+        }
+      },
+    );
+
+    db.execute(
+      "INSERT INTO user (u_firstName, u_lastName, u_email, u_password, u_gender, u_DOB) VALUES (?, ?, ?, ?, ?, ?)",
+      [firstName, lastName, email, password, gender, DOB],
+      (err, results) => {
+        if (err) {
+          return res.status(500).json({ error: "Database error" });
+        }
+        return res.status(201).json({ message: "User created successfully" });
+      },
+    );
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on http://localhost:" + process.env.PORT);
+});
